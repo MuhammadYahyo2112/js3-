d8415a66c58a99701b06aa524958f54707f0aa02
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-04-07 11:10:09 +0300

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,17 @@
+let random = alert("Sizga hozir random raqam chiqazib beradi");
+
+let random1 = Math.round(Math.random() * 100);
+alert("Random son: " + random1 );
+
+let javob = alert("Biz sizga chiqargan random sonni 2ga bolib va 2ga kopaytiramiza");
+
+let kopaytirish =  random1 * 2
+
+let bolsh =  random1 / 2
+
+let foyizi =  random1 % 2
+
+let javob1 = alert("Random sonni 2 ga kopaytirganimizdagi natija : " + kopaytirish + "\n"
+    + "Random sonni 2 ga bolganimizdagi natija : " + bolsh + "\n"
+    + "Random sonni 3 ga bolganimizdagi qoldiq : " + foyizi
+)
